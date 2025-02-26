8b77bdd770ff16437eb9b3600a629775f1a0f5eb
WesselB1234 <stosin007@hotmail.com>
2025-02-26 10:11:48 +0100

    init

---
+++ ProjectDatabaseGroep5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectDatabaseGroep5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
